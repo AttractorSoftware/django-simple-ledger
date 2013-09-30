615d987aa4a5ec9dbd662284b6a2c37013b4ed72
Azamat Tokhtaev <krik123@gmail.com>
2013-09-30 13:58:22 +0600

    Added from_deposit field to the admin interface

--- ledger/admin.py
+++ ledger/admin.py
@@ -4,21 +4,26 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ['agent_from', 'agent_to', 'amount', 'batch_id', 'transaction_type', 'reason', 'date_created']
+    list_display = ['agent_from', 'agent_to', 'amount', 'batch_id', 'transaction_type', 'reason', 'from_deposit',
+                    'date_created']
 
     def agent_from(self, instance):
         return instance.agent_from
+
     agent_from.short_description = _("Money from")
 
     def agent_to(self, instance):
         return instance.agent_to
+
     agent_to.short_description = _("Money to")
 
     def reason(self, instance):
         return instance.reason
+
     reason.short_description = _("Reason")
 
     class Meta:
         model = Transaction
+
 
 admin.site.register(Transaction, TransactionAdmin)
